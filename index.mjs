686084a086704decfc9649cc5a42304533ad08c6
rm <rm@65515107.xyz>
2025-03-06 11:49:31 +0800

    fix(index): 优化 ffmpeg 命令的执行逻辑
    - 在 ffmpeg 命令中添加 "|| true"，以确保即使截图失败也能继续执行脚本
    - 优化了代码格式，提高了可读性

--- index.mjs
+++ index.mjs
@@ -17,7 +17,7 @@ async function main() {
         fs.writeFileSync("./docs/urls.json",JSON.stringify(urls));
         console.log(urls);
         const cmds = urls.map((item,index)=>{
-            return `echo 🎞🎞[${index+1}/${urls.length}] ${item.name} ${item.url}\necho ffmpeg -timeout 10000000 -headers "timeout=10" -i "${item.url}" -vf "select='eq(pict_type,I)',setpts=PTS-STARTPTS" -vsync vfr -q:v 2 -frames:v 1 -y "docs/img/${item.index}.jpg" \nffmpeg -timeout 10000000 -headers "timeout=10" -i "${item.url}" -vf "select='eq(pict_type,I)',setpts=PTS-STARTPTS" -vsync vfr -q:v 2  -frames:v 1 -y "docs/img/${item.index}.jpg"\nffmpeg -i "docs/img/${item.index}.jpg" -vf "scale=iw*0.2:ih*0.2" -y "docs/img/${item.index}_thumbnail.jpg"`
+            return `echo 🎞🎞[${index+1}/${urls.length}] ${item.name} ${item.url}\necho ffmpeg -timeout 10000000 -headers "timeout=10" -i "${item.url}" -vf "select='eq(pict_type,I)',setpts=PTS-STARTPTS" -vsync vfr -q:v 2 -frames:v 1 -y "docs/img/${item.index}.jpg" \nffmpeg -timeout 10000000 -headers "timeout=10" -i "${item.url}" -vf "select='eq(pict_type,I)',setpts=PTS-STARTPTS" -vsync vfr -q:v 2  -frames:v 1 -y "docs/img/${item.index}.jpg || true"\nffmpeg -i "docs/img/${item.index}.jpg" -vf "scale=iw*0.2:ih*0.2" -y "docs/img/${item.index}_thumbnail.jpg"`
         })
         fs.writeFileSync("index.sh",cmds.join("\n"));
     });
