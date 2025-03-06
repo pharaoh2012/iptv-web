daf6211bda76aeff47f93da716e516576ca91ded
rm <rm@65515107.xyz>
2025-03-06 14:38:07 +0800

    feat(main): 优化 ffmpeg 执行结果处理
    - 修改 execAsync 函数，增加错误处理并返回布尔值表示执行结果
    - 在 main 函数中记录每个 URL 的执行结果，并更新到 urls.json
    - 优化日志输出，区分错误信息和正常信息

--- index.mjs
+++ index.mjs
@@ -8,22 +8,24 @@ function execAsync(command) {
     return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
             if (error) {
-                resolve(`执行错误: ${error.message}`);
+                console.error(`❌执行错误: ${error}`);
+                resolve(false);
                 return;
             }
             if (stderr) {
-                resolve(`标准错误输出: ${stderr}`);
+                console.error(`❌❌stderr: ${stderr}`);
+                resolve(false);
                 return;
             }
             console.log(`标准输出:\n${stdout}`);
-            resolve(stdout);
+            resolve(true);
         });
     });
 }
 async function main() {
     // 先创建目录确保存在
     fs.mkdirSync("./docs/img", { recursive: true });
-    
+
     // 删除现有JPG文件
     const imgDir = "./docs/img";
     const files = fs.readdirSync(imgDir);
@@ -32,7 +34,7 @@ async function main() {
         fs.unlinkSync(path.join(imgDir, file));
     });
     console.log(`已删除 ${jpgFiles.length} 个JPG文件`);
-    
+
     const res = await fetch("https://getproxy.deno.dev/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fpharaoh2012%2Fiptv-api%2Foutput%2Fuser_result.txt").then(res => res.text());
 
     let urls = res.split("\n").filter(item => {
@@ -47,15 +49,18 @@ async function main() {
             index: index
         }
     });
-    fs.writeFileSync("./docs/urls.json", JSON.stringify(urls));
+
     // console.log(urls);
     for (let i = 0; i < urls.length; i++) {
         const item = urls[i];
         console.log(`🎞🎞[${i + 1}/${urls.length}] ${item.name} ${item.url}`);
         console.log(`ffmpeg -i "${item.url}" -vf "select='eq(pict_type,I)',setpts=PTS-STARTPTS" -vsync vfr -q:v 2 -frames:v 1 -y "docs/img/${item.index}.jpg"`);
-        await execAsync(`ffmpeg -i "${item.url}" -vf "select='eq(pict_type,I)',setpts=PTS-STARTPTS" -vsync vfr -q:v 2 -frames:v 1 -y "docs/img/${item.index}.jpg"`);
+        let ok = await execAsync(`ffmpeg -i "${item.url}" -vf "select='eq(pict_type,I)',setpts=PTS-STARTPTS" -vsync vfr -q:v 2 -frames:v 1 -y "docs/img/${item.index}.jpg"`);
+        urls[i].ok = ok;
         await execAsync(`ffmpeg -i "docs/img/${item.index}.jpg" -vf "scale=iw*0.2:ih*0.2" -y "docs/img/${item.index}_thumbnail.jpg"`);
     }
+
+    fs.writeFileSync("./docs/urls.json", JSON.stringify(urls));
 
 }
 
